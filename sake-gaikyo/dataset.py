f91c15ce52cfebebe87fd8d4497338d52b9a48ab
uni <yuni.todate@gmail.com>
2022-01-16 17:28:28 +0000

    load as csv

--- sake-gaikyo/dataset.py
+++ sake-gaikyo/dataset.py
@@ -1,13 +1,26 @@
 import pandas as pd
 import tabula
 
+sources = [
+    {
+        "url": 'https://www.nta.go.jp/taxes/sake/shiori-gaikyo/seibun/2020/pdf/001.pdf',
+        "pages": '35-38',
+        "filename": '_gaiyou_2020.csv',
+    }
+]
+kinds = ['一般酒,' '吟醸酒', '純米酒', '本醸造酒']
 
-url = 'https://www.nta.go.jp/taxes/sake/shiori-gaikyo/seibun/2020/pdf/001.pdf'
+def load_data(url, pages):
+    return tabula.read_pdf(url, pages=pages, lattice=True)
 
 
 def main():
-    df = tabula.read_pdf(url)
-    print(df)
+    for s in sources:
+        dfs = load_data(s["url"], s["pages"])
+
+        for i, df in enumerate(dfs):
+            print(df)
+            df.to_csv(f'{kinds[i]}{s["filename"]}')
 
 if __name__ == '__main__':
     main()
