3f427898e9b36cffdd1a4427d4649bbeac210496
uni <yuni.todate@gmail.com>
2022-01-22 05:10:03 +0000

    update poetry

--- sake-gaikyo/app.py
+++ sake-gaikyo/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import altair as alt
 import pandas as pd
 
 import dataset as ds
@@ -19,6 +20,27 @@ def download_button(df):
         key='download-csv'
     )
 
+def taste_plot(df):
+    """
+    light/full: 淡麗、濃醇
+    sweet/dry: 甘口、辛口
+    甘辛度,濃淡度,
+    """
+    c = alt.Chart(df).mark_point(filled=True).encode(
+        x=alt.X('mean(甘辛度)', axis=alt.Axis(title='甘辛度')),
+        y=alt.Y('mean(濃淡度)', axis=alt.Axis(title='濃淡度')),
+        tooltip=['甘辛度', '濃淡度', '都道府県']
+    )
+
+    text = c.mark_text(
+        align='left',
+        baseline='middle',
+        dx=7
+    ).encode(
+        text='都道府県'
+    )
+
+    return c+text
 
 def app():
     st.title("app")
@@ -26,9 +48,18 @@ def app():
     # load
     l = ds.Loader_CSV('./sake-gaikyo/rawdata/test_2020.csv')
     df = l.load()
-    st.subheader("download")
-    download_button(df)
 
+    #
+    kind = st.multiselect("酒種", df["kind"])
+    year = st.multiselect("年", df["year"].unique())
+
+    st.altair_chart(taste_plot(df), use_container_width=True)
+
+
+    prefs = st.multiselect("都道府県", df["県名"].unique())
+
+    st.write("download")
+    download_button(df)
 
 if __name__ == '__main__':
     app()
