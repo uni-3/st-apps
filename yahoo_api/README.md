eede9b905ba49fad5727a572e2a1558b70512521
uni <31564310+uni-3@users.noreply.github.com>
2024-04-09 12:29:35 +0000

    add package

---
+++ yahoo_api/README.md
@@ -0,0 +1,10 @@
+yahoo api で検索したりする画面
+
+- usage
+
+`.streamlit/secrets.toml` を作成し以下の値を設定する
+
+```
+password = "password"
+yahoo_api_key = "dxxxx"
+```
---
+++ yahoo_api/authentication/__init__.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if "password" not in st.session_state:
+            st.session_state["password"] = None
+        elif st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("passwordが誤っています🥺")
+        return False
+    else:
+        # Password correct.
+        return True
